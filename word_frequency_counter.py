3178fcbbb3eb3e59cccafa98e1a98569a9954531
JanaHsen <janahsen5@gmail.com>
2025-03-01 23:34:29 +0200

    added test case for word frequency function

--- word_frequency_counter.py
+++ word_frequency_counter.py
@@ -4,6 +4,8 @@ def word_frequency(sentence):
     for word in words:
         freq_dict[word] = freq_dict.get(word, 0) + 1
     return freq_dict
+sentence = "orange have have two orange"
+print(word_frequency(sentence))
     
      
 
